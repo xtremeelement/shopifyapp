2040e788bd94eda2de0f91c7dfd7138e7965c91e
Jay <Jay@local.local>
2020-08-21 11:27:36 -0400

    setup app.js and index.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
-import '../styles/globals.css'
+import App from 'next/app';
+import Head from 'next/head';
+import{ AppProvider } from '@shopify/polaris';
+import '@shopify/polaris/dist/styles.css';
+import translations from '@shopify/polaris/locales/en.json';
+import { Component } from 'react';
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+class MyApp extends App{
+  render(){
+    const { Component, pageProps } = this.props;
+    return (
+      <>
+        <Head>
+          <title>Test App</title>
+          <meta charset='utf-8'/>
+          <AppProvider i18n={"translations"}>
+            <Component {...pageProps} />
+          </AppProvider>
+        </Head>
+      </>
+    )
+  }
 }
-
-export default MyApp
+export default MyApp;
